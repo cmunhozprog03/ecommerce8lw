f8021fa5da0d04c8a51f227e4ac9aefca16f504e
cmunhozprog03h <cmunhozprog03@hotmail.com>
2021-03-25 19:52:25 -0300

    create project

---
+++ app/Http/Livewire/Companies/CompanyComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Companies;
+
+use Livewire\Component;
+
+class CompanyComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.companies.company-component');
+    }
+}
